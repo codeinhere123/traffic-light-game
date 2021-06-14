7f8c74a562946dba5ff53dd6c823adad3c544b2c
codeinhere123 <71201805+codeinhere123@users.noreply.github.com>
2021-06-14 20:18:32 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+random_light = 
+
+def on_forever():
+    music.play_melody("C D E F G A B C5 ", 120)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+let random_light: number[] = []
+basic.forever(function () {
+    music.playMelody("F G C - A - C - ", 400)
+})
