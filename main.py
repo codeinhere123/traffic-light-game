9c50b6b188fcbaa47d7f84ca85a841fa362e555a
codeinhere123 <71201805+codeinhere123@users.noreply.github.com>
2021-06-15 09:16:28 +0800

    answer code

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-random_light = 
+def on_button_pressed_a():
+    global random_light, rand_time
+    random_light = randint(0, 1)
+    rand_time = randint(2000, 10000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_forever():
-    music.play_melody("C D E F G A B C5 ", 120)
-basic.forever(on_forever)
+def my_function():
+    if random_light == 0:
+        edubitTrafficLightBit.toggle_led(LedColor.RED)
+        basic.pause(rand_time)
+        edubitTrafficLightBit.toggle_led(LedColor.RED)
+    else:
+        edubitTrafficLightBit.toggle_led(LedColor.GREEN)
+        basic.pause(rand_time)
+        edubitTrafficLightBit.toggle_led(LedColor.GREEN)
+edubitIrBit.on_ir_sensor_event(IrEventType.FALL, my_function)
+
+rand_time = 0
+random_light = 0
+for index in range(2):
+    music.play_melody("F G C - A - C - ", 400)
--- main.ts
+++ main.ts
@@ -1,4 +1,26 @@
-let random_light: number[] = []
-basic.forever(function () {
-    music.playMelody("F G C - A - C - ", 400)
+edubitIrBit.onIrSensorEvent(IrEventType.Rise, function () {
+    Game_over = 1
+    music.playMelody("C5 B A G F E D C ", 300)
 })
+input.onButtonPressed(Button.A, function () {
+    Game_over = 0
+    while (Game_over == 0) {
+        random_light = randint(0, 1)
+        rand_time = randint(2000, 10000)
+        if (random_light == 0) {
+            edubitTrafficLightBit.toggleLed(LedColor.Red)
+            basic.pause(rand_time)
+            edubitTrafficLightBit.toggleLed(LedColor.Red)
+        } else {
+            edubitTrafficLightBit.toggleLed(LedColor.Green)
+            basic.pause(rand_time)
+            edubitTrafficLightBit.toggleLed(LedColor.Green)
+        }
+    }
+})
+let rand_time = 0
+let random_light = 0
+let Game_over = 0
+for (let index = 0; index < 2; index++) {
+    music.playMelody("F G C - A - C - ", 400)
+}
